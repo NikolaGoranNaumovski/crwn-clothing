481b33819ef66804c65e5b1cb64cbfdc90b9715a
nikola.naumovski <nikola.naumovski@halicea.com>
2023-07-23 17:07:59 +0200

    Add authentication with firebase

--- src/routes/sign-in/index.jsx
+++ src/routes/sign-in/index.jsx
@@ -1,7 +1,20 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase";
+import { createUserDocumentFromAuth } from "../../utils/firebase/firebase";
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    try {
+      const { user } = await signInWithGooglePopup();
+      createUserDocumentFromAuth(user);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
   return (
     <div>
       <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
     </div>
   );
 };
--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDtLVqgXTnzd-s4pdSVkxuNt-KYD-b27jM",
@@ -7,7 +8,7 @@ const firebaseConfig = {
   projectId: "crwn-db-f9c5a",
   storageBucket: "crwn-db-f9c5a.appspot.com",
   messagingSenderId: "81646096286",
-  appId: "1:81646096286:web:cc139d9c0f282f163e908f"
+  appId: "1:81646096286:web:cc139d9c0f282f163e908f",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -15,9 +16,29 @@ const firebaseApp = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-    prompt: "select_account"
-})
+  prompt: "select_account",
+});
 
 export const auth = getAuth();
 
-export const signInWithGooglePopup = () => signInWithPopup(auth,  provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  const userExists = userSnapshot.exists();
+
+  if (!userExists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.error(`Error creating the user ${error.message}`);
+    }
+  }
+};
